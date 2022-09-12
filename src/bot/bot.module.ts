82b33872080b18002795c241afc7bed03d4197da
Doan Ngoc Ha <doanha2111999@gmail.com>
2022-09-12 11:17:38 +0700

    push code

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,20 +1,20 @@
 import { DiscordModule } from "@discord-nestjs/core";
 import { Module } from "@nestjs/common";
+import { DiscoveryModule } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
-import DailyCommand from "./commands/daily.command";
+import { DailyCommand } from "./commands/daily.command";
 import { BotGateway } from "./events/bot.gateway";
 import { Daily } from "./models/daily.entity";
-import { Order } from "./models/order.entity";
-import { PlayCommand } from "./slash-commands/play.slashcommand";
-import { PlaylistCommand } from "./slash-commands/playlist.slashcommand";
+import { PlaySlashCommand } from "./slash-commands/play.slashcommand";
+import { PlaylistSlashCommand } from "./slash-commands/playlist.slashcommand";
 
 @Module({
   imports: [
     DiscordModule.forFeature(),
+    DiscoveryModule,
     TypeOrmModule.forFeature([Daily]),
-    TypeOrmModule.forFeature([Order]),
   ],
-  providers: [PlayCommand, PlaylistCommand, BotGateway],
+  providers: [PlaySlashCommand, PlaylistSlashCommand, BotGateway, DailyCommand],
 })
 export class BotModule {}
