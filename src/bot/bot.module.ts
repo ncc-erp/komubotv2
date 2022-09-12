0341f65e99e3ec0ed1bdabcd232e2a4850de1aa8
Doan Ngoc Ha <doanha2111999@gmail.com>
2022-09-12 11:30:57 +0700

    push command order leave holiday

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -4,8 +4,14 @@ import { DiscoveryModule } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { DailyCommand } from "./commands/daily.command";
+import holidayCommand from "./commands/holiday.command";
+import LeaveCommand from "./commands/leave.command";
+import OrderCommand from "./commands/ncc8.command";
 import { BotGateway } from "./events/bot.gateway";
 import { Daily } from "./models/daily.entity";
+import { Holiday } from "./models/holiday.entity";
+import { Leave } from "./models/leave.entity";
+import { Order } from "./models/order.entity";
 import { PlaySlashCommand } from "./slash-commands/play.slashcommand";
 import { PlaylistSlashCommand } from "./slash-commands/playlist.slashcommand";
 
@@ -14,7 +20,11 @@ import { PlaylistSlashCommand } from "./slash-commands/playlist.slashcommand";
     DiscordModule.forFeature(),
     DiscoveryModule,
     TypeOrmModule.forFeature([Daily]),
+    TypeOrmModule.forFeature([Order]),
+    TypeOrmModule.forFeature([Leave]),
+    TypeOrmModule.forFeature([Holiday]),
+
   ],
-  providers: [PlaySlashCommand, PlaylistSlashCommand, BotGateway, DailyCommand],
+  providers: [PlaySlashCommand, PlaylistSlashCommand, BotGateway, DailyCommand, OrderCommand, holidayCommand, LeaveCommand],
 })
 export class BotModule {}
