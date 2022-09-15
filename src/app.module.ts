81d5eee99300093d36d6a7c7876bb7af7c1e4b0c
Nguyen Quan <quan.nguyendinh@ncc.asia>
2022-09-15 13:47:51 +0700

    create service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GatewayIntentBits } from 'discord.js';
 
 import { BotModule } from './bot/bot.module';
+import { CheckListModule } from './bot/commands/Checklist/checklist.module';
 
 @Module({
   imports: [
--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,9 +1,15 @@
 import { DiscordModule } from "@discord-nestjs/core";
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { DiscoveryModule } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { BotController } from "./bot.controller";
 import { BotService } from "./bot.service";
+import { ChecklistCommand } from "./commands/checklist.command";
+import { CheckListController } from "./commands/Checklist/checklist.controller";
+import { CheckListModule } from "./commands/Checklist/checklist.module";
+import { CheckListService } from "./commands/Checklist/checklist.service";
+import { CompantripCommand } from "./commands/Companytrip/companytrip.command";
+import { CompanyModule } from "./commands/Companytrip/companytrip.module";
 
 import { DailyCommand } from "./commands/daily.command";
 import holidayCommand from "./commands/holiday.command";
@@ -14,28 +20,31 @@ import { Daily } from "./models/daily.entity";
 import { Holiday } from "./models/holiday.entity";
 import { Leave } from "./models/leave.entity";
 import { Order } from "./models/order.entity";
-import { OrderService } from "./service/order.service";
 import { PlaySlashCommand } from "./slash-commands/play.slashcommand";
 import { PlaylistSlashCommand } from "./slash-commands/playlist.slashcommand";
 import { UntilService } from "./untils/until.service";
+
 
 @Module({
   imports: [
     DiscordModule.forFeature(),
     DiscoveryModule,
     TypeOrmModule.forFeature([Daily, Order, Leave, Holiday]),
+    forwardRef(() => CheckListModule),
+    CompanyModule
   ],
   providers: [
     PlaySlashCommand,
     PlaylistSlashCommand,
+    ChecklistCommand,
+    CompantripCommand,
     BotGateway,
     DailyCommand,
     OrderCommand,
     holidayCommand,
     LeaveCommand,
-    BotService,
-    OrderService,
-    UntilService,
+    BotService, 
+    UntilService
   ],
   controllers: [BotController],
 })
