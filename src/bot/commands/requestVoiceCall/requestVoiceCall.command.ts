dc416769fa0cdf6955ae7eecb2074a7a558f9ac3
hoang.nguyendinh <dhoang1406@gmail.com>
2023-10-03 15:06:05 +0700

    update *call

--- src/bot/commands/requestVoiceCall/requestVoiceCall.command.ts
+++ src/bot/commands/requestVoiceCall/requestVoiceCall.command.ts
@@ -1,4 +1,4 @@
-import { Client, Message } from "discord.js";
+import { ChannelType, Client, Message } from "discord.js";
 import { CommandLine, CommandLineClass } from "src/bot/base/command.base";
 import { RequestVoiceCallService } from "./requestVoiceCall.service";
 import { KomubotrestService } from "src/bot/utils/komubotrest/komubotrest.service";
@@ -12,26 +12,28 @@ export class RequestVoiceCallCommand implements CommandLineClass {
   constructor(
     private readonly requestVoiceCallService: RequestVoiceCallService,
     private komubotrestService: KomubotrestService
-  ) {}
+  ) { }
   async execute(message: Message, args, client: Client) {
     try {
       if (args[0]) {
         const emailUser = args[0];
         let authorId = message.author.id;
         const user = await this.requestVoiceCallService.getDataUser(emailUser);
-
+        let rooms;
         if (!user)
           return message.reply(`Wrong Email!`).catch((err) => {
             this.komubotrestService.sendErrorToDevTest(client, authorId, err);
           });
-
-        const rooms = await this.requestVoiceCallService.getAllVoiceChannel(
-          client
-        );
-
+        if (message.member.voice.channel &&
+          message.member.voice.channel.type === ChannelType.GuildVoice) {
+          rooms = [message.member.voice.channel.id];
+        } else {
+          rooms = await this.requestVoiceCallService.getAllVoiceChannel(
+            client
+          );
+        }
         let userMentions = await client.users.fetch(user.userId);
         let author = await client.users.fetch(authorId);
-
         if (rooms.length > 0) {
           await this.requestVoiceCallService.sendLinkToJoinCall(
             userMentions,
